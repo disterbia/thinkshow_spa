a59d0e3cabbf4e0afc13dea22f06e1266145a5fc
hyni24 <hyni24@gmail.com>
2023-02-14 16:39:59 +0900

    스낵바 색상 변경

--- wholesaler-user/lib/app/widgets/snackbar.dart
+++ wholesaler-user/lib/app/widgets/snackbar.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wholesaler_user/app/constants/colors.dart';
 
-mSnackbar({required String message, String? actionText, VoidCallback? onPressed, int duration = 2}) {
+mSnackbar(
+    {required String message,
+    String? actionText,
+    VoidCallback? onPressed,
+    int duration = 2}) {
   final snackBar = SnackBar(
+    backgroundColor: Color.fromARGB(210, 43, 43, 43),
     content: Text(message),
     padding: EdgeInsets.only(top: 10, bottom: 10, left: 20),
     action: SnackBarAction(
